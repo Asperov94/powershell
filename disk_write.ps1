7d921e5e3bfb6eb9ade145df997b6fb6d71d057f
Евгений Матешев <matesheved@mos.ru>
2020-01-30 15:19:14 +0300

    script create iso disk usb

---
+++ disk_write.ps1
@@ -0,0 +1,3 @@
+$Results = Get-Disk | Where-Object BusType -eq USB | Out-GridView -Title 'Select USB Drive to Format' -OutputMode Single | Clear-Disk -RemoveData -RemoveOEM -Confirm:$false -PassThru | New-Partition -UseMaximumSize -IsActive -AssignDriveLetter | Format-Volume -FileSystem FAT32 ; $Volumes = (Get-Volume).Where({$_.DriveLetter}).DriveLetter
+Mount-DiskImage -ImagePath D:\загрузочный_образ.ISO
+$ISO = (Compare-Object -ReferenceObject $Volumes -DifferenceObject (Get-Volume).Where({$_.DriveLetter}).DriveLetter).InputObject
